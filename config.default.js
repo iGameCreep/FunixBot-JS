0b30a7b45c89cd31fb3f0c9d2d83339a8a4bbdac
iGameCreep <gamecreepbs@gmail.com>
2023-02-14 21:14:13 +0100

    First commit

---
+++ config.default.js
@@ -0,0 +1,9 @@
+module.exports = {
+    app: {
+        token: 'BOT TOKEN',
+        global: false,
+        prefix: "!",
+        guild: '161940168620638208', // Actual FunixGaming guild ID
+        devs: ["696753471650660412"], // Put your ID here
+    },
+};
---
+++ main.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+global.client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.MessageContent
+    ],
+   disableMentions: 'everyone',
+});
+
+client.config = require('./config');
+
+client.createID = require('./Fonctions/createID')
+
+require('./src/loader');
+require('./src/events');
+
+client.login(client.config.app.token);
